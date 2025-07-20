c41bce10d9c21b29629547da53e2dff1e76fdfb8
steadysamwise4 <davenportsam44@gmail.com>
2025-07-19 22:28:43 -0500

    Player class created and drawn to the game screen

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
 from constants import *
 
+from player import Player
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     print("Starting Asteroids!")
     while True:
@@ -11,7 +16,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
